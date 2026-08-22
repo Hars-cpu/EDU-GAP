777a75e9c92b35aaedc8f7823f38e3b8ef2ad108
anmol567-lab <guptajianm123@gmail.com>
2026-08-23 03:37:15 +0530

    work done

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+
+const app = express();
+
+// Connect MongoDB
+ connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+// Test
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
