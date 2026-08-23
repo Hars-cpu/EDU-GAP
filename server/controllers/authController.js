68d2cfea7c550ac819cff16feafde006cfcbd74a
anmol567-lab <guptajianm123@gmail.com>
2026-08-24 00:46:10 +0530

    Update cookie settings for JWT in authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -144,8 +144,8 @@ export const login = async (req, res) => {
     // Send JWT in cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
